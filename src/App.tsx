05d051a0b9ba1cd03f15b6d96bdd7f02297800c9
tamaokishie <shie0041@gmail.com>
2025-02-07 00:57:46 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+export const App = () => {
+
+  const [users, setUsers] = useState<string[]>([]);
+
+  const handlePost = async () => {
+    try {
+        const response = await fetch("http://localhost:8080/user", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name: "田村三郎"})
+        });
+        if (!response.ok) {
+          throw new Error('ネットワークレスポンスが正常ではありません');
+        }
+        handleGet();
+      } catch(error) {
+        console.error('フェッチ操作に問題が発生しました:', error);
+      }
+  };
+
+  const handleGet = async () => {
+    try {
+        const response = await fetch("http://localhost:8080/users");
+        if (!response.ok) {
+          throw new Error('ネットワークレスポンスが正常ではありません');
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch(error) {
+        console.error('フェッチ操作に問題が発生しました:', error);
+      }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <button className='button' onClick={handlePost}>
+        田村三郎を追加
+      </button>
+      <button className='button' onClick={handleGet}>
+        ユーザー一覧を取得
+      </button>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user}</li>
+        ))}
+      </ul>
     </div>
   );
-}
-
-export default App;
+};
